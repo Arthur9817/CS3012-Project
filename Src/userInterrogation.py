76d11e6f9e11ec133c59e40793077bcc42935352
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-12-01 20:04:21 +0000

    re-commited file

---
+++ Src/userInterrogation.py
@@ -0,0 +1,17 @@
+from github import Github
+import getpass
+ user = False
+while(not user):
+    username = input("Enter your Github Username:\n")
+    password = getpass.getpass(prompt='Enter your password:\n')
+    try:
+        g = Github(username, password)
+        user = True
+    except:
+        print("Try again")
+ # print username
+user = g.get_user()
+print(user.login + "\n")
+ # print repos
+for repo in g.get_user().get_repos():
+    print(repo.name)
