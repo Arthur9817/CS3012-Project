d124c8bf8f35667f20ef16c8ab56fee63527263a
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-12-01 19:39:32 +0000

    interrogates github api

--- Src/gitVisual.py
+++ Src/gitVisual.py
@@ -1,16 +1,21 @@
-from github import Github
-import getpass
+import requests
+import csv
+
 
 def main():
-user = False
-    while(not user):
-        username = input("Enter your Github Username:\n")
-        password = getpass.getpass(prompt='Enter your password:\n')
-        try:
-            g = Github(username, password)
-            user = True
-        except:
-            print("Try again")
+    repo_list = []
+    stars_list = []
+
+    repos = requests.get('https://api.github.com/search/repositories?q=stars%3A>1&sort=stars&order=desc&page=&per_page=10&rel="next"').json()
+    for item in repos['items']:
+        repo_list.append(item['name'])
+        stars_list.append(item['stargazers_count'])
+
+    with open('data.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Repo', 'Stars'])
+        for i in range(len(repo_list)):
+            writer.writerow([repo_list[i],stars_list[i]])
 
 if __name__== "__main__":
   main()
